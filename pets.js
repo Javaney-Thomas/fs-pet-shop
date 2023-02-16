0f4595461ea632d0b9b247ac2cb76eedf79fa9f0
Javaney <javaneyt@icloud.com>
2023-02-15 21:38:14 -0600

    Day 1 fleshing out the file creation using node syntax

---
+++ pets.js
@@ -0,0 +1,3 @@
+//console.error(node pets.js [read | create | update | destroy])
+console.log(typeof process.argv[2])//All the arguments in the method are strings starting at 2
+var petnum = process.argv[2]
